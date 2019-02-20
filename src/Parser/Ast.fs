4564abcc7d5fd9c3bd31ff591dd02b6b513862af
maccth <christophermacca@hotmail.co.uk>
2019-02-20 14:55:38 +0000

    [parser]: progress on parsing literals

--- src/Parser/Ast.fs
+++ src/Parser/Ast.fs
@@ -1,10 +1,27 @@
 module Bilbo.Parser.Ast
+open System.Linq.Expressions
 
-type Program = Declaration list
+type Program = ProgramUnit list
 
-and Declaration =
+and ProgramUnit =
+    | Statement of Statement
+
+and Statement =
     | TypeDeclaration of TypeName * Attribute list
-    // TODO: add inheritance types
+    | ExpressionStatement of ExpressionStatement
 
+and ExpressionStatement =
+    | AssignmentExpression of VariableIdentifier * Expression
+
+and Expression =
+    | LiteralExpression of Literal
+  
+and Literal =
+    | StringLiteral of string
+    | FloatLiteral of float
+    | IntLiteral of int
+    | BoolLiteral of bool
+      
+and VariableIdentifier = string
 and TypeName = string
 and Attribute = string 
--- src/Parser/Parser.fs
+++ src/Parser/Parser.fs
@@ -2,26 +2,70 @@ module Bilbo.Parser.Parser
 open FParsec
 open Ast
 
+let keywords =
+    [
+        "type"
+    ] |> Set.ofList
+
+
+let isKeyword w = keywords.Contains w
+
 let ws = spaces
 
 let str s = pstring s .>> spaces
+/// Exact version of str, no spaces consumed.
+let stre s = pstring s
+
+let pnKeyword : Parser<unit, unit> =
+    let kws = Set.toList keywords
+    let pKws = List.map str kws
+    notFollowedBy (choice pKws)
 
 let id : Parser<string, unit> =
     let firstChar c = isLetter c || c = '_'
     let middleChar c = firstChar c || isDigit c
     let upToLastChar = many1Satisfy2 firstChar middleChar
     let lastChar = (str "'" <|> str "")
-    pipe2 upToLastChar lastChar (+)
+    pnKeyword >>. (pipe2 upToLastChar lastChar (+))
 
 let csvIds1 = sepBy1 id (str ",")
 
 let pTypeDeclaration =
-    let ctor = (fun _ id _ attrs -> (id, attrs) |> TypeDeclaration)
+    let ctor = fun _ name _ attrs -> (name, attrs) |> TypeDeclaration
     pipe4 (str "type") id (str "=") csvIds1 ctor
 
+let pStrLiteral : Parser<Literal, unit> =
+    let chars = manySatisfy (fun c -> c <> '"')
+    between (stre "\"") (stre "\"") chars |>> StringLiteral
 
+let pIntLiteral : Parser<Literal, unit> = pint32 |>> int |>> IntLiteral
 
-let pProgram = choice [pTypeDeclaration]
+let pFloatLiteral : Parser<Literal, unit> = pfloat |>> FloatLiteral
+
+let pBoolLiteral : Parser<Literal, unit> =
+    (str "True" <|> str "False")
+    |>> function
+        | t when t="True" -> true
+        | _ -> false
+    |>> BoolLiteral
+
+let pLiteral = choice [pStrLiteral; pFloatLiteral; pIntLiteral; pBoolLiteral] |>> LiteralExpression
+
+let pExpression = choice [pLiteral]
+
+let pAssignmentExpression  =
+    let ctor = fun var _ expr ->  (var, expr) |> AssignmentExpression
+    pipe3 id (str "=") pExpression ctor
+
+// Top level parsers
+
+let pExpressionStatement = choice [pAssignmentExpression] |>> ExpressionStatement
+
+let pStatement = choice [pTypeDeclaration; pExpressionStatement] |>> Statement
+
+let pProgramUnit = pStatement
+
+let pProgram =  choice [pProgramUnit] |> many1
 
 let pFile = ws >>. pProgram .>> ws .>> eof
 
