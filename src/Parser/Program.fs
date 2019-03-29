7903989727c748869af5e80cee50b33138fc3bf9
maccth <christophermacca@hotmail.co.uk>
2019-03-29 20:04:47 +0000

    [parser]: added parsing REPL

--- src/Parser/Program.fs
+++ src/Parser/Program.fs
@@ -1,24 +1,31 @@
 ﻿module Bilbo.Parser.Main
 open System
 open FParsec
-
 open Ast
 open Parser
+
+let outputParsedResult res =
+    match res with
+    | Success(res', _s, _p) ->
+        printf "The AST of the input is:\n%A\n" res'
+    | Failure(msg, err, u) ->
+        printfn "%s" msg
+        printfn "%A" err 
+        printfn "%A" u
 
 [<EntryPoint>]
 let main (argv : string []) =
     match argv.Length with
     | l when l <> 1 ->
-        printfn "Pass in a file as the first and only argument"
-        exit 1
+        printfn "Starting parsing REPL"
+        while true do
+            printf "\n~> "
+            let line = Console.ReadLine()
+            let result = pBilboStr line
+            outputParsedResult result
+        0
     | _ ->
         let file =  argv.[0]
         let result = pBilboFile file System.Text.Encoding.UTF8
-        match result with
-        | Success(res, _s, _p) ->
-            printf "The AST of the input file is:\n%A\n" res
-        | Failure(msg, err, u) ->
-            printfn "%s" msg
-            printfn "%A" err 
-            printfn "%A" u
+        outputParsedResult result
         0
