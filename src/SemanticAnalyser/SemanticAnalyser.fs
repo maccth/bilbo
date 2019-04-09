994de0f1eb0bacdd5ffb84ae0d21ddc443af40d1
maccth <christophermacca@hotmail.co.uk>
2019-04-09 17:26:34 +0100

    [semantic analyser]: distinct module for symbol table

--- src/SemanticAnalyser/SemanticAnalyser.fs
+++ src/SemanticAnalyser/SemanticAnalyser.fs
@@ -1,30 +1,7 @@
 ﻿module Bilbo.SemanticAnalyser.SemanticAnalyser
 
 open Bilbo.Parser.Ast
-open Bilbo.SemanticAnalyser.Value
-
-type SymbolTable = {
-    types       : Map<string * string,  TypeDef>; 
-    transforms  : Map<string * string,  TransformDef>;
-    variables   : Map<string * string,  Value>;
-    namespaces  : Map<string,           string>;
-}
-
-let consSymbolTable = {
-    types       = Map.empty;
-    transforms  = Map.empty;
-    variables   = Map.empty;
-    namespaces  = Map.empty;
-}
-
-let addType (st : SymbolTable) (nspace,tname) tdef =
-    {st with types = Map.add (nspace,tname) tdef st.types}
-
-let addTransform (st : SymbolTable) (nspace,tname) tdef =
-    {st with transforms = Map.add (nspace,tname) tdef st.transforms}
-
-let addNamespace (st : SymbolTable) nspace fp =
-    {st with namespaces = Map.add nspace fp st.namespaces}
+open Bilbo.SemanticAnalyser.SymbolTable
 
 // TODO: Create Bilbo error types
 type BilboError = string
@@ -120,7 +97,7 @@ let rec aProgram (st : SymbolTable) astIn astOut : Analysed<Program> =
         Ok(st, Some astOut')
 
 let bilboSemanticAnalyser (astIn : Program) =
-    aProgram consSymbolTable astIn []
+    aProgram emptySymbolTable astIn []
 
 let bilboSemanticAnalyserPrint (astIn : Program) =
     let out = bilboSemanticAnalyser astIn
