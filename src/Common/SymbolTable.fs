2fe4a88c67929f0aeb0315f0772bffd1fec0879d
maccth <christophermacca@hotmail.co.uk>
2019-04-12 14:10:56 +0100

    [evaluator]: evaluating type definitions

--- src/Common/SymbolTable.fs
+++ src/Common/SymbolTable.fs
@@ -2,39 +2,40 @@ module Bilbo.Common.SymbolTable
 
 open Bilbo.Common.Value
 open Bilbo.Common.Ast
-open Bilbo.Common
-open Bilbo.Common
 
 module SymbolTable =
     type Table = Map<Id, Meaning>
 
     and Meaning =
         | Value of Value
-        | SymbolTable of Table
+        | SymbolTable of SymbolType * Table 
+
+    and SymbolType =
+        | Namespace
+        | Object
 
     let empty : Table
         = Map.empty
     
     let addType (symTabs:Table list) nLst id (def : TypeDef) =
+        let rec add (st : Table) (nLst : Namespace list) (id : Id) (def : Meaning) =
+            match nLst with
+            | [] ->
+                Map.add id def st
+            | Top :: rest ->
+                add st rest id def
+            | Name n :: rest ->
+                match Map.tryFind n st with
+                | Some (SymbolTable(Namespace, stCurrent)) ->                 
+                    let stUpdated = add stCurrent rest id def |> fun s -> SymbolTable(Namespace,s)
+                    Map.add n stUpdated st
+                | _ ->
+                    let stNew = (add empty rest id def) |> fun s -> (Namespace,s) |> SymbolTable
+                    Map.add n stNew st
         let def' = def |> Type |> Value
         let rnLst = List.rev nLst
         match rnLst with
         | e :: nLst' ->
-            let rec add (st : Table) (nLst : Namespace list) (id : Id) (def : Meaning) =
-                match nLst with
-                | Name n :: rest ->
-                    match Map.tryFind n st with
-                    | Some(SymbolTable stCurrent) ->
-                        let stNew = add empty rest id def
-                        let st' = Map.fold (fun acc k v -> Map.add k v acc) stCurrent stNew
-                        st'
-                    | _ ->
-                        let nestedSt = add empty rest id def |> SymbolTable
-                        Map.add n nestedSt st
-                | Top :: rest ->
-                    add st rest id def
-                | [] ->
-                    Map.add id def st
             match symTabs with
             | st :: rest ->
                 let st' = add st rnLst id def'
@@ -42,5 +43,6 @@ module SymbolTable =
             | [] ->
                 let st' = add empty rnLst id def'
                 [st']
+        | [] -> failwithf "Will not happen. All program units exist in a namespace." 
 
 type ProgramSymbols = SymbolTable.Table list
--- src/Evaluator/Main.fs
+++ src/Evaluator/Main.fs
@@ -7,6 +7,6 @@ open Bilbo.Evaluator.Evaluator
 [<EntryPoint>]
 let main argv =
     let repl code = code |> bilboStringParser |> bilboEvaluatorPrint
-    let normal code = code |> bilboStringParser |> bilboEvaluatorPrint
+    let normal code = code |> bilboParser |> bilboEvaluatorPrint
     cli argv "Bilbo evaluator REPL" repl normal
     0
