5fc2249f652d47fd4c0229769944f1ef90603363
maccth <christophermacca@hotmail.co.uk>
2019-04-18 11:40:43 +0100

    [evaluator]: added Match type so intention (of composing matches) is more obvious; bug fix for string addition which was not composing matches correctly

--- src/Evaluator/BinaryExpressions.fs
+++ src/Evaluator/BinaryExpressions.fs
@@ -6,62 +6,76 @@ open Bilbo.Common.Error
 open System
 open Bilbo.Common
 
-let (<.|.>) first (second : Lazy<'T option>) =
+type Match<'T> =
+    | Matched of 'T
+    | NoMatch
+
+let (<.|.>) (first : Match<'T>) (second : Lazy<Match<'T>>) =
     match first with
-    | Some s -> s |> Some
-    | None ->
+    | Matched s -> s |> Matched
+    | NoMatch ->
         match second.Force() with
-        | Some s -> s |> Some
-        | None -> None
+        | Matched s -> s |> Matched
+        | NoMatch -> NoMatch
 
-let noneReplace rep x =
-    match x with
-    | Some x' -> x'
-    | None -> rep
+module Match =
+    let underlie instead m =
+        match m with
+        | Matched m' -> m'
+        | NoMatch -> instead
 
-let intFloat ops iiFun ifFun fiFun ffFun erFun =
+    let compose first second =
+        match first with
+        | Matched x -> x |> Matched
+        | NoMatch ->
+            match second with
+            | Matched y -> y |> Matched
+            | NoMatch -> NoMatch
+
+    let lazyCompose first second =
+        first <.|.> second
+
+let intFloat ops iiFun ifFun fiFun ffFun =
     match ops with
-    | Error e -> e |> Error |> Some
+    | Error e -> e |> Error |> Matched
     | Ok (Value lhs', Value rhs') ->
         match lhs',rhs' with
-        | Int x, Int y -> (iiFun x y) |> Value |> Ok |> Some
-        | Int x, Float y -> (ifFun x y) |> Value |> Ok |> Some
-        | Float x, Int y -> (fiFun x y) |> Value |> Ok |> Some
-        | Float x, Float y -> (ffFun x y) |> Value |> Ok |> Some
-        // TODO: consider changing to None and only adding error
-        // at the end. Will allow easier composition of matches
-        | _ -> erFun |> Error |> Some
-    | _ -> None
+        | Int x, Int y -> (iiFun x y) |> Value |> Ok |> Matched
+        | Int x, Float y -> (ifFun x y) |> Value |> Ok |> Matched
+        | Float x, Int y -> (fiFun x y) |> Value |> Ok |> Matched
+        | Float x, Float y -> (ffFun x y) |> Value |> Ok |> Matched
+        | _ -> NoMatch
+    | _ -> NoMatch
 
-let intFloatStr ops iiFun ifFun fiFun ffFun ssFun erFun =
+let intFloatStr ops iiFun ifFun fiFun ffFun ssFun =
     let str = lazy(
         match ops with
-        | Error e -> e |> Error |> Some
+        | Error e -> e |> Error |> Matched
         | Ok (Value lhs', Value rhs') ->
             match lhs',rhs' with
-            | String x, String y -> (ssFun x y) |> Value |> Ok |> Some
-            | _ -> erFun |> Error |> Some
-        | _ -> None)        
-    (intFloat ops iiFun ifFun fiFun ffFun erFun) <.|.> str
+            | String x, String y -> (ssFun x y) |> Value |> Ok |> Matched
+            | _ -> NoMatch
+        | _ -> NoMatch)        
+    (intFloat ops iiFun ifFun fiFun ffFun) <.|.> str
         
-let intFloat2 ops iiFun ifFun fiFun ffFun erFun =
+let intFloat2 ops iiFun ifFun fiFun ffFun =
     let iiFun2 x y = (iiFun x y) |> Int
     let ifFun2 x y = (ifFun x y) |> Float
     let fiFun2 x y = (fiFun x y) |> Float
     let ffFun2 x y = (ffFun x y) |> Float
-    intFloat ops iiFun2 ifFun2 fiFun2 ffFun2 erFun
+    intFloat ops iiFun2 ifFun2 fiFun2 ffFun2
 
-let intFloatStr2 ops iiFun ifFun fiFun ffFun ssFun erFun =
+let intFloatStr2 ops iiFun ifFun fiFun ffFun ssFun =
     let ssFun2 x y = (ssFun x y) |> Value.String
-    let str2 =
+    let str2 = lazy(
         match ops with
-        | Error e -> e |> Error |> Some
+        | Error e -> e |> Error |> Matched
         | Ok (Value lhs', Value rhs') ->
             match lhs',rhs' with
-            | String x, String y -> (ssFun2 x y) |> Value |> Ok |> Some
-            | _ -> erFun |> Error |> Some
-        | _ -> None        
-    intFloat2 ops iiFun ifFun fiFun ffFun erFun
+            | String x, String y -> (ssFun2 x y) |> Value |> Ok |> Matched
+            | _ -> NoMatch
+        | _ -> NoMatch)       
+    (intFloat2 ops iiFun ifFun fiFun ffFun) <.|.> str2
 
 let operatorErr = 
     // TODO: Implement!
@@ -69,128 +83,128 @@ let operatorErr =
     |> ImplementationError
 
 let plusRules (ops : BilboResult<Meaning * Meaning>) : BilboResult<Meaning> =
-    intFloatStr2 ops (+) (fun x y -> float(x) + y) (fun x y -> x + float(y)) (+) (+) operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloatStr2 ops (+) (fun x y -> float(x) + y) (fun x y -> x + float(y)) (+) (+)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
     // TODO: Add graph-based plus operations
 
 let minusRules ops =
-    intFloat2 ops (-) (fun x y -> float(x) - y) (fun x y -> x - float(y)) (-) operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat2 ops (-) (fun x y -> float(x) - y) (fun x y -> x - float(y)) (-)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
     // TODO: Add graph-based minus operations
 
 let timesRules ops =
-    intFloat2 ops (*) (fun x y -> float(x) * y) (fun x y -> x * float(y)) (*) operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat2 ops (*) (fun x y -> float(x) * y) (fun x y -> x * float(y)) (*)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let zeroCheck ops =
     match ops with
-    | Error e -> e |> Error |> Some
+    | Error e -> e |> Error |> Matched
     | Ok (Value lhs', Value rhs') ->
         match lhs',rhs' with
         | (_, Int y) when y=0 ->
-            "Cannot divide or modulo by zero" |> ValueError |> Error |> Some
+            "Cannot divide or modulo by zero" |> ValueError |> Error |> Matched
         | (_, Float y) when y=0.0 ->
-            "Cannot divide or modulo by zero" |> ValueError |> Error |> Some
-        | _ -> None
-    | _ -> None  
+            "Cannot divide or modulo by zero" |> ValueError |> Error |> Matched
+        | _ -> NoMatch
+    | _ -> NoMatch  
 
 let divideRules ops =
     let z = zeroCheck ops
-    let eval = lazy(intFloat2 ops (/) (fun x y -> float(x) / y) (fun x y -> x / float(y)) (/) operatorErr)
+    let eval = lazy(intFloat2 ops (/) (fun x y -> float(x) / y) (fun x y -> x / float(y)) (/))
     z <.|.> eval
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
     
 let moduloRules ops =
     let z = zeroCheck ops
-    let eval = lazy(intFloat2 ops (%) (fun x y -> float(x) % y) (fun x y -> x % float(y)) (%) operatorErr)
+    let eval = lazy(intFloat2 ops (%) (fun x y -> float(x) % y) (fun x y -> x % float(y)) (%))
     z <.|.> eval
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let powRules ops =
     let iiFun = fun x y -> float(x) ** float(y) |> Float
     let ifFun = fun x y -> float(x) ** y |> Float
     let fiFun = fun x y -> x ** float(y) |> Float    
     let ffFun = fun x y -> x ** y |> Float    
-    intFloat ops iiFun ifFun fiFun ffFun operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat ops iiFun ifFun fiFun ffFun
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let ltRules ops =
     let lt = fun x y -> (x < y) |> Bool
     let ifFun = fun x y -> (float(x) < y) |> Bool
     let fiFun = fun x y -> x < float(y) |> Bool
-    intFloat ops lt ifFun fiFun lt operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat ops lt ifFun fiFun lt
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let lteqRules ops =
     let lteq = fun x y -> (x <= y) |> Bool
     let ifFun = fun x y -> (float(x) <= y) |> Bool
     let fiFun = fun x y -> x <= float(y) |> Bool
-    intFloat ops lteq ifFun fiFun lteq operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat ops lteq ifFun fiFun lteq
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let gtRules ops =
     // Unpacking the result of lteqRules and inverting it would take as many line...
     let gt = fun x y -> (x > y) |> Bool
     let ifFun = fun x y -> (float(x) > y) |> Bool
     let fiFun = fun x y -> x > float(y) |> Bool
-    intFloat ops gt ifFun fiFun gt operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat ops gt ifFun fiFun gt
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let gteqRules ops =
     let gteq = fun x y -> (x >= y) |> Bool
     let ifFun = fun x y -> (float(x) >= y) |> Bool
     let fiFun = fun x y -> x >= float(y) |> Bool
-    intFloat ops gteq ifFun fiFun gteq operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat ops gteq ifFun fiFun gteq
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let equalsRules ops =
     // TODO: Add equality for non-primative (structural types) types
     let eq = fun x y -> (x = y) |> Bool
     let ifFun = fun x y -> (float(x) = y) |> Bool
     let fiFun = fun x y -> x = float(y) |> Bool
-    intFloat ops eq ifFun fiFun eq operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat ops eq ifFun fiFun eq
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let notEqualsRules ops =
     // TODO: Add non-equality for non-primative (structural types) types
     let neq = fun x y -> (x <> y) |> Bool
     let ifFun = fun x y -> (float(x) <> y) |> Bool
     let fiFun = fun x y -> x <> float(y) |> Bool
-    intFloat ops neq ifFun fiFun neq operatorErr
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    intFloat ops neq ifFun fiFun neq
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let boolean ops binOp =
     let booleanTrue (v : Value) =
         // TODO: Add graph pattern matching `and` and `or` operartions for pos and neg graph
         // and non-primative types 
         match v with
-        | Int x -> x<>0 |> Some
-        | Float x -> x<>0.0 |> Some
-        | String x -> x<>"" |> Some
-        | Bool x -> x |> Some
-        | _ -> None
+        | Int x -> x<>0 |> Matched
+        | Float x -> x<>0.0 |> Matched
+        | String x -> x<>"" |> Matched
+        | Bool x -> x |> Matched
+        | _ -> NoMatch
     match ops with
-    | Error e -> e |> Error |> Some
+    | Error e -> e |> Error |> Matched
     | Ok (Value lhs, Value rhs) ->
         let lhs' = booleanTrue lhs
         let rhs' = booleanTrue rhs
         match lhs',rhs' with
-        | Some l', Some r' -> binOp l' r' |> Some
-        | _ -> None
-    | _ -> None
+        | Matched l', Matched r' -> binOp l' r' |> Matched
+        | _ -> NoMatch
+    | _ -> NoMatch
 
 let andRules (ops : BilboResult<Meaning*Meaning>) =
     boolean ops (fun x y -> (x && y) |> Bool |> Value |> Ok)
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let orRules (ops : BilboResult<Meaning*Meaning>) =
     boolean ops (fun x y -> (x || y) |> Bool |> Value |> Ok)
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 let xorRules (ops : BilboResult<Meaning*Meaning>) =
     let xor x y = (x && (not y)) || ((not x) && y)
     boolean ops (fun x y -> (xor x y) |> Bool |> Value |> Ok)
-    |> noneReplace ("Not implemented yet." |> ImplementationError |> Error)
+    |> Match.underlie ("Not implemented yet." |> ImplementationError |> Error)
 
 
 (**
