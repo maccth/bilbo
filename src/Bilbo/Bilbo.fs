e01b19b9fd91de429633fd0359f7d12817aeb3f6
maccth <christophermacca@hotmail.co.uk>
2019-06-16 12:10:13 +0100

    [evaluator]: added bilbo entry point to the system rather using evaluator entry point

---
+++ src/Bilbo/Bilbo.fs
@@ -0,0 +1,12 @@
+﻿module Bilbo.Bilbo.Bilbo
+
+open Bilbo.Evaluator.Main
+
+// This is the top level entry point for the whole interpreter
+// While it just adds another level of indirection to the evaluator this means that the interpreter
+//  is accessed from a Bilbo.dll rather than an Evaluator.dll which is a component of the system
+
+[<EntryPoint>]
+let main argv =
+    bilboCli argv
+    0
--- src/Evaluator/Main.fs
+++ src/Evaluator/Main.fs
@@ -5,10 +5,13 @@ open Bilbo.Common.SymbolTable
 open Bilbo.Parser.Parser
 open Bilbo.Evaluator.Evaluator
 
-[<EntryPoint>]
-let main argv =
+let bilboCli argv =
     let file code = code |> bilboParser |> bilboEvaluator
     let repl code syms = code |> bilboStringParser |> fun ast ->  bilboEvaluatorSymsIn ast syms
     let titleStr = "Bilbo REPL v1.0" + "\n\n" + "\"All we have to decide is what to do with the time that is given to us.\" - Gandalf\n"
     cli argv titleStr  file repl Symbols.Empty
+
+[<EntryPoint>]
+let main argv =
+    bilboCli argv
     0
